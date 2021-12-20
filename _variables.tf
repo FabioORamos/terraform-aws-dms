dd856921c01d29415f8cda99e591d459969a3318
Fabio Ramos <fabio.d.o.ramos@gmail.com>
2021-12-21 09:47:11 +1100

    change variable

--- _variables.tf
+++ _variables.tf
@@ -60,24 +60,24 @@ variable "engine_version" {
   default = "none"
 }
 
-variable "replication_instance_class" {
-  description = "The compute and memory capacity of the replication instance as specified by the replication instance class"
-  default = "none"
-}
+# variable "replication_instance_class" {
+#   description = "The compute and memory capacity of the replication instance as specified by the replication instance class"
+#   default = "none"
+# }
 
-variable "replication_instance_id" {
-  description = "The replication instance identifier"
-}
+# variable "replication_instance_id" {
+#   description = "The replication instance identifier"
+# }
 
-variable "preferred_maintenance_window" {
-  description = "The weekly time range during which system maintenance can occur, in Universal Coordinated Time (UTC)"
-  default = "none"
-}
+# variable "preferred_maintenance_window" {
+#   description = "The weekly time range during which system maintenance can occur, in Universal Coordinated Time (UTC)"
+#   default = "none"
+# }
 
-variable "availability_zone" {
-  description = "The EC2 Availability Zone that the replication instance will be created in"
-}
+# variable "availability_zone" {
+#   description = "The EC2 Availability Zone that the replication instance will be created in"
+# }
 
-variable "subnet_ids" {
-  description = "A list of the EC2 subnet IDs for the subnet group"
-}
+# variable "subnet_ids" {
+#   description = "A list of the EC2 subnet IDs for the subnet group"
+# }
